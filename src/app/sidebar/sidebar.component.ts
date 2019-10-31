88334db840d930f1f21603a009a2ca0355dc4e93
nildiert <niljordan23@gmail.com>
2019-10-31 14:31:05 -0500

    [FEATURE] Added some features to show the picture of the user

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http/http.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { PeopleService } from '../services/people/people.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+
+  photo: string;
+  constructor(
+    private httpService: HttpService,
+    private router: Router,
+    private authService: AuthService,
+    private peopleService: PeopleService
+    ) {
+    if (!this.authService.logIn) {
+      this.router.navigate(['signin']);
+    }
+  }
 
   ngOnInit() {
+    this.userData();
+  }
+
+  userData() {
+    this.peopleService.getUser(localStorage.user_id)
+    .subscribe((response: any) => {
+      localStorage.setItem('profile_pic', response.profile_pic);
+      this.photo = localStorage.profile_pic;
+    });
   }
 
 }
